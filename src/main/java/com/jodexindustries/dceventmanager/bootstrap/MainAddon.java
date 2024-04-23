8c63c3be9589c0e9461d53963115bfc83164c4c3
Jodexx <0984134q@gmail.com>
2024-04-23 21:51:48 +0300

    2.2.0.8 api adaptation

--- src/main/java/com/jodexindustries/dceventmanager/bootstrap/MainAddon.java
+++ src/main/java/com/jodexindustries/dceventmanager/bootstrap/MainAddon.java
@@ -3,7 +3,7 @@ package com.jodexindustries.dceventmanager.bootstrap;
 import com.jodexindustries.dceventmanager.command.MainCommand;
 import com.jodexindustries.dceventmanager.config.Config;
 import com.jodexindustries.dceventmanager.listener.EventListener;
-import com.jodexindustries.donatecase.api.SubCommandManager;
+import com.jodexindustries.donatecase.api.CaseManager;
 import com.jodexindustries.donatecase.api.addon.JavaAddon;
 import org.bukkit.Bukkit;
 
@@ -11,25 +11,27 @@ import java.io.File;
 
 public final class MainAddon extends JavaAddon implements Main {
     public static MainAddon instance;
+    private CaseManager api;
     public static Tools t;
     public Config config;
 
     @Override
     public void onEnable() {
         instance = this;
+        api = getCaseAPI();
         t = new Tools(this, getLogger());
         config = new Config(getDataFolder());
         if(!new File(getDataFolder(), "config.yml").exists()) {
             saveResource("config.yml", false);
         }
         Bukkit.getServer().getPluginManager().registerEvents(new EventListener(), getDonateCase());
-        SubCommandManager.registerSubCommand("dcem", new MainCommand(this));
+        api.getSubCommandManager().registerSubCommand("dcem", new MainCommand(this));
         t.loadEvents();
     }
 
     @Override
     public void onDisable() {
-        SubCommandManager.unregisterSubCommand("dcem");
+        api.getSubCommandManager().unregisterSubCommand("dcem");
     }
 
     @Override
--- src/main/java/com/jodexindustries/dceventmanager/bootstrap/MainPlugin.java
+++ src/main/java/com/jodexindustries/dceventmanager/bootstrap/MainPlugin.java
@@ -3,28 +3,30 @@ package com.jodexindustries.dceventmanager.bootstrap;
 import com.jodexindustries.dceventmanager.command.MainCommand;
 import com.jodexindustries.dceventmanager.config.Config;
 import com.jodexindustries.dceventmanager.listener.EventListener;
-import com.jodexindustries.donatecase.api.SubCommandManager;
+import com.jodexindustries.donatecase.api.CaseManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MainPlugin extends JavaPlugin implements Main {
     public static MainPlugin instance;
+    private CaseManager api;
     public Config config;
     public Tools t;
 
     @Override
     public void onEnable() {
+        api = new CaseManager(this);
         instance = this;
         t = new Tools(this, getLogger());
         config = new Config(getDataFolder());
         saveDefaultConfig();
         getServer().getPluginManager().registerEvents(new EventListener(), this);
-        SubCommandManager.registerSubCommand("dcem", new MainCommand(this));
+        api.getSubCommandManager().registerSubCommand("dcem", new MainCommand(this));
         t.loadEvents();
     }
 
     @Override
     public void onDisable() {
-        SubCommandManager.unregisterSubCommand("dcem");
+        api.getSubCommandManager().unregisterSubCommand("dcem");
     }
     @Override
     public Config getAddonConfig() {
