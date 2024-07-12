4fad48a493ffd494c6312d4d38755c11002edcc8
Jodexx <0984134q@gmail.com>
2024-07-12 20:04:47 +0300

    Add Placeholder class

---
+++ src/main/java/com/jodexindustries/dceventmanager/data/Placeholder.java
@@ -0,0 +1,27 @@
+package com.jodexindustries.dceventmanager.data;
+
+public class Placeholder {
+    private final String name;
+    private final String method;
+    public Placeholder(String name, String method) {
+
+        this.name = name;
+        this.method = method;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    @Override
+    public String toString() {
+        return "Placeholder{" +
+                "name='" + name + '\'' +
+                ", method='" + method + '\'' +
+                '}';
+    }
+}
